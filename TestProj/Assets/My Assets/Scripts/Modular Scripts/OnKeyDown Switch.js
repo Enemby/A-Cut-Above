26c3b8fade852ff7b1ce3a0b7e24f18799f7c32f
Enemby <Enemby@users.noreply.github.com>
2020-03-13 04:10:38 -0600

    rebuild up to date
    had to restore from backup, we're back to full functionality now

---
+++ TestProj/Assets/My Assets/Scripts/Modular Scripts/OnKeyDown Switch.js
@@ -0,0 +1,19 @@
+﻿//iterate through a list of disabled objects. Once we've reached the end of the list, load a scene
+var myObjects : GameObject[];
+var myIndex : int = 0;
+var myScene : String;
+function Update(){
+	if(Input.anyKeyDown){
+		IterateObjects();
+	}
+}
+function IterateObjects(){
+	if(myIndex < myObjects.Length-1){
+		myIndex+=1;
+		myObjects[myIndex-1].active = false;
+		myObjects[myIndex].active = true;
+	}
+	else{
+		Application.LoadLevel(myScene);
+	}
+}
